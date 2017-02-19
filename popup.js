935aba4ffbe15f0621bf967d10df7c8d403bb729
Dastagiri Reddy <reddy.dastagiri386@gmail.com>
2017-02-19 11:45:08 -0600

    revert chnages to previous (size of icon) and removed logic for 'fake'

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
 
 var common_words="the, it is, we all, a, an, by, to, you, me, he, she, they, we, how, it, i, are, to, for, of, with, at"; 
-var negation_words ="fake";
-var flag = 0;
 
 function getUncommon(sentence, common) {
     var wordArr = sentence.match(/\w+/g),
@@ -15,12 +13,7 @@ function getUncommon(sentence, common) {
     for ( i = 0; i < common.length; i++ ) {
         commonObj[ common[i].trim() ] = true;
     }
-
-    //create list of negation words
-    negation = negation.split(',');
-    for ( i = 0; i < negation.length; i++ ) {
-        negationObj[ negation[i].trim() ] = true;
-    }
+ 
 
     for ( i = 0; i < wordArr.length; i++ ) {
         word = wordArr[i].trim().toLowerCase();
@@ -60,7 +53,7 @@ function httpGetAsync(theUrl, callback)
 
 				var results = responseDiv.querySelectorAll('.r > a');
 				console.log("RAHUL CLASS START");
-				console.log(String(results[0].attributes.href));
+				//console.log(String(results[0].attributes.href));
 				console.log("RAHUL CLASS END");
 				
 				//alert("The first URL: " + String(results[0]));//(results[0].attributes.href));
@@ -81,34 +74,19 @@ function httpGetAsync(theUrl, callback)
 			{	//alert(xmlHttp.responseText);
 				//alert("The actual response obtained. Second level");
 				console.log(xmlHttp.response);
-				if (flag == 0) {
-					if( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif") )
-					{
-						//Change html body accordingly
-						document.body.innerHTML = "<img style='width:210px;height:210px' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif' >"; //"False";
-					}
-					else if ( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif") )
-					{
-						document.body.innerHTML = "<img height='210' width='210' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif' >"; //"True";
-					}
-					else
-					{
-						document.body.innerHTML = "<img height='210' width='210' src='http://worldartsme.com/images/unsure-clipart-1.jpg' >"//"Sorry. Truth check does not have sufficient info. You decide!";
-					}
-				} else {
-					if( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif") )
-					{
-						//Change html body accordingly
-						document.body.innerHTML = "<img style='width:210px;height:210px' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif' >"; //"False";
-					}
-					else if ( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif") )
-					{
-						document.body.innerHTML = "<img height='210' width='210' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif' >"; //"True";
-					}
-					else
-					{
-						document.body.innerHTML = "<img height='210' width='210' src='http://worldartsme.com/images/unsure-clipart-1.jpg' >"//"Sorry. Truth check does not have sufficient info. You decide!";
-					}
+				
+				if( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif") )
+				{
+					//Change html body accordingly
+					document.body.innerHTML = "<img style='width:210px;height:210px' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif' >"; //"False";
+				}
+				else if ( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif") )
+				{
+					document.body.innerHTML = "<img height='210' width='210' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif' >"; //"True";
+				}
+				else
+				{
+					document.body.innerHTML = "<img height='210' width='210' src='http://worldartsme.com/images/unsure-clipart-1.jpg' >"//"Sorry. Truth check does not have sufficient info. You decide!";
 				}
 			}
 		}
