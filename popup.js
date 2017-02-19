5a62f95abe03aa6898b67099de8aff0e80520ddf
Dastagiri Reddy <reddy.dastagiri386@gmail.com>
2017-02-19 11:30:36 -0600

    added negation words and reverse notion when these present

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 
 var common_words="the, it is, we all, a, an, by, to, you, me, he, she, they, we, how, it, i, are, to, for, of, with, at"; 
+var negation_words ="fake";
+var flag = 0;
 
 function getUncommon(sentence, common) {
     var wordArr = sentence.match(/\w+/g),
         commonObj = {},
+        negationObj = {},
         uncommonArr = [],
 		uncommonStr = "",
         word, i;
@@ -13,11 +16,19 @@ function getUncommon(sentence, common) {
         commonObj[ common[i].trim() ] = true;
     }
 
+    //create list of negation words
+    negation = negation.split(',');
+    for ( i = 0; i < negation.length; i++ ) {
+        negationObj[ negation[i].trim() ] = true;
+    }
+
     for ( i = 0; i < wordArr.length; i++ ) {
         word = wordArr[i].trim().toLowerCase();
         if ( !commonObj[word] ) {
-            //uncommonArr.push(word);
+            //uncommonArr.push(word)
 			uncommonStr = uncommonStr + " " + word;
+			if (negationObj[word])
+				flag = 1;
         }
     }
 
@@ -70,18 +81,34 @@ function httpGetAsync(theUrl, callback)
 			{	//alert(xmlHttp.responseText);
 				//alert("The actual response obtained. Second level");
 				console.log(xmlHttp.response);
-				if( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif") )
-				{
-					//Change html body accordingly
-					document.body.innerHTML = "<img style='width:210px;height:210px' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif' >"; //"False";
-				}
-				else if ( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif") )
-				{
-					document.body.innerHTML = "<img height='210' width='210' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif' >"; //"True";
-				}
-				else
-				{
-					document.body.innerHTML = "<img height='210' width='210' src='http://worldartsme.com/images/unsure-clipart-1.jpg' >"//"Sorry. Truth check does not have sufficient info. You decide!";
+				if (flag == 0) {
+					if( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif") )
+					{
+						//Change html body accordingly
+						document.body.innerHTML = "<img style='width:210px;height:210px' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif' >"; //"False";
+					}
+					else if ( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif") )
+					{
+						document.body.innerHTML = "<img height='210' width='210' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif' >"; //"True";
+					}
+					else
+					{
+						document.body.innerHTML = "<img height='210' width='210' src='http://worldartsme.com/images/unsure-clipart-1.jpg' >"//"Sorry. Truth check does not have sufficient info. You decide!";
+					}
+				} else {
+					if( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif") )
+					{
+						//Change html body accordingly
+						document.body.innerHTML = "<img style='width:210px;height:210px' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif' >"; //"False";
+					}
+					else if ( xmlHttp.response.includes("http://www.snopes.com/app/themes/snopes-theme/dist/images/det-green.gif") )
+					{
+						document.body.innerHTML = "<img height='210' width='210' src='http://www.snopes.com/app/themes/snopes-theme/dist/images/det-red.gif' >"; //"True";
+					}
+					else
+					{
+						document.body.innerHTML = "<img height='210' width='210' src='http://worldartsme.com/images/unsure-clipart-1.jpg' >"//"Sorry. Truth check does not have sufficient info. You decide!";
+					}
 				}
 			}
 		}
